08ab07420df2807ba994d8f296eb1795c2151841
davidson <dehpacifico@gmail.com>
2024-04-26 11:10:51 -0300

    adicionar equals and hash code para entidades

--- src/main/java/com/devsuperior/dscommerce/entities/OrderItemPk.java
+++ src/main/java/com/devsuperior/dscommerce/entities/OrderItemPk.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+import java.util.Objects;
+
 @Embeddable
 public class OrderItemPk {
 
@@ -32,5 +34,21 @@ public class OrderItemPk {
 
     public void setOrder(Order order) {
         this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItemPk that = (OrderItemPk) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(order);
+        result = 31 * result + Objects.hashCode(product);
+        return result;
     }
 }
